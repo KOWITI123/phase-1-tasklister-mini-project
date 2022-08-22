c30cb15f8cd1f2b4911001fb4a708855636fe279
Allan <allan.kowiti@student.moringaschool.com>
2022-08-22 22:53:10 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // your code here 
+  const form = document.getElementById('create-task-form');
+  const task = document.getElementById('tasks');
+
+  form.addEventListener('submit', createTask);
+  task.addEventListener('click', deleteTask)
+  function createTask(e){
+    e.preventDefault();
+
+    const newTaskDescription = document.getElementById('new-task-description').value;
+    console.log(newTaskDescription);
+
+    const li = document.createElement('li');
+
+    li.appendChild(document.createTextNode(newTaskDescription));
+    const deleteBtn = document.createElement('button');
+
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    li.appendChild(deleteBtn);
+
+    task.appendChild(li);
+
+  }
+
+  function deleteTask(e) {
+    if (e.target.nodeName === 'BUTTON') {
+      if(confirm('Are you sure?')) {
+        let li = e.target.parentElement;
+        task.removeChild(li);
+      }
+    }
+
+  }
 });
+    
+    
+    
+
